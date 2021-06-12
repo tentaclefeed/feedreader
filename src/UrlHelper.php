c2fa863b340c97c31368a73c37399b8585360674
Marc Reichel <mail@marcreichel.de>
2021-06-13 01:05:44 +0200

    🐛 Bugfix

--- src/UrlHelper.php
+++ src/UrlHelper.php
@@ -11,6 +11,10 @@ class UrlHelper
 {
     public static function makeAbsoluteUrl(UriInterface $effectiveUri, string $path): string
     {
+        if (preg_match('~https?://~i', $path)) {
+            return $path;
+        }
+
         if (Str::startsWith($path, '/')) {
             $scheme = $effectiveUri->getScheme();
             $host = $effectiveUri->getHost();
@@ -18,10 +22,6 @@ class UrlHelper
             return $scheme . '://' . $host . ($port !== null ? ':' . $port : '') . $path;
         }
 
-        if (!preg_match("~^https?://~i", $path)) {
-            return $effectiveUri . $path;
-        }
-
-        return $path;
+        return $effectiveUri . $path;
     }
 }
